512541ceabddef7fdcee31e11506cc6ddb55f631
arigova <arigova@smartdatatech.io>
2026-05-28 14:17:26 +0300

    feat: add create and delete workout mutations

---
+++ src/hooks/useCreateWorkout.ts
@@ -0,0 +1,12 @@
+import {createWorkout} from '@/api/workouts';
+import {useMutation, useQueryClient} from '@tanstack/react-query';
+
+export function useCreateWorkout() {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: createWorkout,
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({queryKey: ['workouts']});
+    },
+  });
+}
